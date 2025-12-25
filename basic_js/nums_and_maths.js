d9a56381507cf8cc8106ebf29edb7e74a8db4b0d
heyysvm <imshivam339@gmail.com>
2025-12-25 18:01:40 +0000

    math and nums

---
+++ basic_js/nums_and_maths.js
@@ -0,0 +1,32 @@
+// const score = 400
+
+// const balance = new Number (499)
+
+// console.log(score)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 24.4564
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// +++++++++++ M A T H S +++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.23))
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+
+
+// console.log(Math.ceil(Math.random()*10))
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random()*(max-min+1)+min))
---
+++ basic_js/strings.js
@@ -0,0 +1,33 @@
+
+// const name = "Shibam"
+// const repoCount = 50 
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+const gameName =new String ("BattleGround-India-20-25")
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// 
+
+// const anotherString = gameName.slice(-9,9)
+// console.log(anotherString);
+
+// const newStringOne = "   shivam   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim())
+
+// const url = "https://shivam.com/shivam%20singh"
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes("shivam"))
+ 
+// let splitted = gameName.split('-')
+// console.log(splitted);
+// console.log(splitted.join("*"))
