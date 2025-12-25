327be40b164c0830cd2efd031093a7d4759370f4
heyysvm <imshivam339@gmail.com>
2025-12-25 06:35:27 +0000

    new1

---
+++ basic_js/conversionOperations.js
@@ -0,0 +1,27 @@
+let a = "122a"
+
+console.log(typeof a)
+console.log(typeof(a))
+
+let valueInNumber = Number(a)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "12" => 12
+// "122a" => NaN 
+// true  => 1 ; false =>0
+// undefined = NaN
+// null => NaN
+
+let x = 1 
+console.log(Boolean(x))
+
+// 1 => true , 0 => false
+// "" => false 
+//"string" => ture 
+
+let anyNumber = 21
+
+let stringNumber = String(anyNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
