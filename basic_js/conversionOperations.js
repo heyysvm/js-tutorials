08cafb80710506639b840c5c4b055d9f5b556d4a
heyysvm <imshivam339@gmail.com>
2025-12-25 06:46:53 +0000

    newtopic1

--- basic_js/conversionOperations.js
+++ basic_js/conversionOperations.js
@@ -1,27 +1,61 @@
-let a = "122a"
+// let a = "122a"
 
-console.log(typeof a)
-console.log(typeof(a))
+// console.log(typeof a)
+// console.log(typeof(a))
 
-let valueInNumber = Number(a)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(a)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-// "12" => 12
-// "122a" => NaN 
-// true  => 1 ; false =>0
-// undefined = NaN
-// null => NaN
+// // "12" => 12
+// // "122a" => NaN 
+// // true  => 1 ; false =>0
+// // undefined = NaN
+// // null => NaN
 
-let x = 1 
-console.log(Boolean(x))
+// let x = 1 
+// console.log(Boolean(x))
 
-// 1 => true , 0 => false
-// "" => false 
-//"string" => ture 
+// // 1 => true , 0 => false
+// // "" => false 
+// //"string" => ture 
 
-let anyNumber = 21
+// let anyNumber = 21
 
-let stringNumber = String(anyNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// let stringNumber = String(anyNumber)
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+// let value = 4
+//  let negValue = -value 
+//  console.log(negValue)
+
+// // operations 
+ 
+// // console.log(2+2);
+// // console.log(2-2);
+// // console.log(2*2);
+// // console.log(2/2);
+// // console.log(2%2);
+
+// let str1 = "hello"
+// let str2 ="world"
+// console.log(str1+str2)
+
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log((3+4)*5%3)
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1 = num2 = num3 
+ 
+// num1 = num2 = num3 = 2+2 
+
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
+
