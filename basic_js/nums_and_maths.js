a1ada25df263571b689d146b4f0f7e345abaac27
heyysvm <imshivam339@gmail.com>
2025-12-25 19:41:37 +0000

    basic

--- basic_js/nums_and_maths.js
+++ basic_js/nums_and_maths.js
@@ -26,7 +26,7 @@
 
 // console.log(Math.ceil(Math.random()*10))
 
-const min = 10 
-const max = 20 
+// const min = 10 
+// const max = 20 
 
-console.log(Math.floor(Math.random()*(max-min+1)+min))
+// console.log(Math.floor(Math.random()*(max-min+1)+min))
