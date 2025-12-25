31c97d5319af982a3c17c14ad14c959302c3429e
heyysvm <imshivam339@gmail.com>
2025-12-25 20:00:15 +0000

    m...

---
+++ basic_js/arrays.js
@@ -0,0 +1,30 @@
+//  A R R A Y 
+
+ const myArr = [1,2,3,4,5,6]
+ const myHeros =["superman","gojo"]
+ const myArr2 = new Array(1,2,3,4)
+
+//  console.log(myArr[0]);
+ 
+ // ARRAY METHODS 
+
+// myArr.push(67)
+// myArr.pop()
+// myArr.unshift()
+// myArr.shift()
+// console.log(myArr);
+// console.log(myArr.indexOf(3)) 
+
+
+// slice , splice 
+ 
+//  console.log("A ", myArr)
+ 
+//  const myn1 = myArr.slice(1,3)
+
+//  console.log(myn1)
+//  console.log("B : ",myArr)
+
+//   const myn2 = myArr.splice(1,3)
+//    console.log("C : ",myArr)
+//    console.log(myn2)
