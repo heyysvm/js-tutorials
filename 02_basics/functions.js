66b6ad71f05e8f99e7dce0927d8df48d01122330
heyysvm <imshivam339@gmail.com>
2025-12-27 16:06:54 +0000

    fn

--- 02_basics/functions.js
+++ 02_basics/functions.js
@@ -18,11 +18,42 @@
 
 // console.log(result)
 
-function loginMessage (username){
-    if (username===undefined){
-        console.log("Please enter a username !!")
-        return
-    }
-    return `${username}just logged in`
-}
-console.log(loginMessage("shivam"))
+// function loginMessage (username){
+//     if (username===undefined){
+//         console.log("Please enter a username !!")
+//         return
+//     }
+//     return `${username}just logged in`
+// }
+// console.log(loginMessage("shivam"))
+
+// function calculateCartPrice(...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,400,500,200))
+
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(200,400,500,200))
+
+// const user ={
+//     username : "shviam",
+//     price:199
+// }
+
+ // *********
+
+//  function handleObject (anyobject){
+//     console.log(`Username is ${anyobject.username}and price is ${anyobject.price }`)
+//  }
+//  handleObject(user)
+
+// // **********
+
+// const myNewArray = [200,300,400,500]
+
+// function returnSecondValue (getArray){
+//     return getArray[1]
+// }
+// console.log(returnSecondValue(myNewArray))
