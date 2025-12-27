0e5ccc188aecbcce97025057e0e94776c032e14d
heyysvm <imshivam339@gmail.com>
2025-12-27 17:13:43 +0000

    iife

---
+++ 02_basics/iife.js
@@ -0,0 +1,15 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION ( IIFE)
+  
+// global scope ek pollution ko hatane k liye iife ka use krte hai
+
+// (function chai (){
+// named IIFE
+//     console.log(`Databases connected`)
+// }) (); //  USING TWO IIFE CONTINOUSLY ..  semicolon needed to stop the iife execution !!
+
+// ()() // 1st parantheses is function call and second is  execution of the funciton .
+
+// ((name) => 
+// {  // UNNAME IIFE 
+//     console.log(`DB CONNECTED TWO ${name}`)
+// })('shviam')
