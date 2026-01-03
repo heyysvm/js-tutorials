6ac3ccaa8aedb1b5f96ab084dbfaa86da3c0f574
Shivam <imshivam339@gmail.com>
2026-01-03 16:39:16 +0000

    .

---
+++ OOPS/object.js
@@ -0,0 +1,18 @@
+function multiplyby5(num){
+    this.num = num 
+    return num*5
+}
+
+multiplyby5.power = 2 
+console.log(multiplyby5(5));
+console.log(multiplyby5.power);
+console.log(multiplyby5.prototype);
+
+function createUser(username,score){
+    this.username  = username 
+    this.score = score 
+}
+
+createUser.prototype.increment = function(){
+    score++
+}
