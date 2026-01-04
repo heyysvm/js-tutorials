e0b0d2db6d17d283f139c4d0dfd96b03e2c02149
Shivam <imshivam339@gmail.com>
2026-01-04 18:08:50 +0000

    getter/setter

--- OOPS/getter_setter.js
+++ OOPS/getter_setter.js
@@ -3,13 +3,21 @@ class User {
         this.email = email,
         this.password = password
     }
+    get email(){
+        return this._email.toUpperCase()
+    }
+     set email(value){
+         this._email =value 
+    }
     get password(){
-        return this._password.toUpperCase()
+
+        return this._password.toUpperCase()  // return as customized when someone accessing the passwords
+
     }
     set password(value){
-      this._password= value.toUpperCase()
+      this._password= value  // set as normal pass in databasess 
     }
 }
 
-const shivam = new User ("shivam@gmail.com","123")
-console.log(shivam.password)
+const shivam = new User ("shivam@gmail.com","1abc23")
+console.log(shivam.email)
