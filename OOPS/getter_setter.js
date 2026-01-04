e736638fe6ece8f2d44c10f9d56f0226aa6ca132
Shivam <imshivam339@gmail.com>
2026-01-04 17:58:27 +0000

    .

--- OOPS/getter_setter.js
+++ OOPS/getter_setter.js
@@ -3,6 +3,12 @@ class User {
         this.email = email,
         this.password = password
     }
+    get password(){
+        return this._password.toUpperCase()
+    }
+    set password(value){
+      this._password= value.toUpperCase()
+    }
 }
 
 const shivam = new User ("shivam@gmail.com","123")
